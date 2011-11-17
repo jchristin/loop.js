03a2311f8c7b633be832f10a23e68ff6246a10c2
FredBrach <fredbrach@californiacomputing.net>
2011-11-17 20:20:30 +0100

    Update loop.js

--- loop.js
+++ loop.js
@@ -3,7 +3,7 @@
 // loop.js v1.0.0
 // http://julbaxter.github.com/loop.js/
 //
-// Copyright (c) 2011, Julien Christin, Frederic Brachfogel
+// Copyright (c) 2011, Julien Christin
 // Licensed under the Apache 2.0 License.
 //
 ////////////////////////////////
